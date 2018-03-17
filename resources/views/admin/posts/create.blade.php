21c45954cc8394ca21ca9396402993251a1453a3
Saranda <sarandasadiku2@gmail.com>
2018-03-17 16:10:51 +0100

    first commit

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+    <div class="card">
+        <div class="card-header">
+            Create a new post
+        </div>
+
+        <div class="card-body">
+            <form class="" action="/post/store" method="post">
+                {{ csrf_field() }}
+
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" name="title" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="title">Featured image</label>
+                    <input type="file" name="featured" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="title">Content</label>
+                    <textarea name="content" id="content" rows="5" cols="5" class="form-control"></textarea>
+                </div>
+                <div class="form-group text-center">
+                    <button type="submit" class="btn btn-success">Store Post</button>
+                </div>
+
+            </form>
+        </div>
+    </div>
+@stop
